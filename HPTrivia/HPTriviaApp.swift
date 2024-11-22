b5f83ebec2d97448fa90265bb16f02c4c948e0fe
iviejo_genasys <iviejo@genasys.com>
2024-11-22 10:35:45 -0300

    Initial Commit

---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by Kinamic Kinamic on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
